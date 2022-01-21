98dfaefcc97856ec39daf4d9cad68dfcd6db893b
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2022-01-21 22:15:18 +0200

    removed persistor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import { store, persistor } from './store/store';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <App />
-            </PersistGate>
+            <App />
         </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
--- src/store/store.js
+++ src/store/store.js
@@ -1,35 +1,8 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/reducer';
-
-const middleware = [
-    ...getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
-];
-
-const todosPersistConfig = {
-    key: 'contacts',
-    storage,
-    blacklist: ['filter'],
-};
 
 export const store = configureStore({
     reducer: {
-        contacts: persistReducer(todosPersistConfig, contactsReducer),
+        contacts: contactsReducer,
     },
-    middleware,
 });
-export const persistor = persistStore(store);
